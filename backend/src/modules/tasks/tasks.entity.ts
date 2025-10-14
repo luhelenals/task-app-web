d86072c820d8bc06e57f45b61570a34b83a7c785
Luiza Santos <luizahelena.lsantos@gmail.com>
2025-10-14 17:20:07 -0300

    feat: user e task entity

---
+++ backend/src/modules/tasks/tasks.entity.ts
@@ -0,0 +1,50 @@
+// src/modules/tasks/task.entity.ts
+
+import { User } from '../users/users.entity';
+import { 
+    Entity, 
+    PrimaryGeneratedColumn, 
+    Column, 
+    CreateDateColumn,
+    ManyToOne 
+} from 'typeorm';
+
+// Status possíveis para uma tarefa
+export enum TaskStatus {
+    PENDENTE = 'PENDENTE',
+    EM_ANDAMENTO = 'EM_ANDAMENTO',
+    CONCLUIDA = 'CONCLUIDA',
+}
+
+@Entity({ name: 'tasks' })
+export class Task {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    titulo: string;
+
+    @Column({ nullable: true })
+    descricao: string;
+
+    @Column({
+        type: 'enum',
+        enum: TaskStatus,
+        default: TaskStatus.PENDENTE,
+    })
+    status: TaskStatus;
+
+    @CreateDateColumn() // cria data automaticamente
+    data_criacao: Date;
+
+    @Column({ type: 'datetime', nullable: true }) // A data de conclusão pode ser nula
+    data_conclusao: Date | null;
+
+    // --- Relacionamento ---
+    // muitas tarefas -> um usuário
+    @ManyToOne(() => User, (user) => user.tasks, { eager: false })
+    owner: User;
+
+    @Column()
+    ownerId: number;
+}
---
+++ backend/src/modules/users/users.entity.ts
@@ -0,0 +1,28 @@
+import { Task } from '../tasks/tasks.entity';
+import { 
+    Entity, 
+    PrimaryGeneratedColumn, 
+    Column, 
+    CreateDateColumn, 
+    OneToMany 
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column({ select: false })
+    password: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    // --- Relacionamento ---
+    // um user -> muitas tarefas
+    @OneToMany(() => Task, (task) => task.owner, { cascade: true })
+    tasks: Task[];
+}
