72960bd3bf73384a82eae291d4d33fd59b9e3b7a
Luiza Santos <luizahelena.lsantos@gmail.com>
2025-10-15 00:28:52 -0300

    feat: task service

--- backend/src/modules/tasks/tasks.service.ts
+++ backend/src/modules/tasks/tasks.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async findAllByUserId(userId: number) {
+        return this.prisma.task.findMany({
+            where: { ownerId: userId },
+        });
+    }
+
+    async findById(id: number) {
+        return this.prisma.task.findUnique({
+            where: { id },
+        });
+    }
+
+    async create(data: Prisma.TaskCreateInput) {
+        return this.prisma.task.create({
+            data,
+        });
+    }
+
+    async update(id: number, data: Prisma.TaskUpdateInput) {
+        return this.prisma.task.update({
+            where: { id },
+            data,
+        });
+    }
+
+    async delete(id: number) {
+        return this.prisma.task.delete({
+            where: { id },
+        });
+    }
+
+}
