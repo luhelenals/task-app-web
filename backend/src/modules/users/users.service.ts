69ae93ac352ec002cccfffe43411d354632dab6d
Luiza Santos <luizahelena.lsantos@gmail.com>
2025-10-14 22:48:51 -0300

    feat: configuração do user service

--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { User, Prisma } from '@prisma/client';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private prisma: PrismaService) {}
+
+    async findByEmail(email: string): Promise<User | null> {
+        return this.prisma.user.findUnique({
+            where: { email },
+        });
+    }
+
+    async findByEmailWithPassword(email: string): Promise<User | null> {
+        return this.prisma.user.findUnique({
+            where: { email },
+            select: { id: true, email: true, password: true, createdAt: true },
+        });
+    }
+
+    async create(data: Prisma.UserCreateInput): Promise<User> {
+        return this.prisma.user.create({
+            data,
+        });
+    }
+}
