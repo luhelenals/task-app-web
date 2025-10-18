618877ef4f4ac9842dcd45768f0c0629a1436e62
Luiza Santos <luizahelena.lsantos@gmail.com>
2025-10-17 23:59:14 -0300

    feat: mudanças no formulário

--- frontend/src/components/taskForm.js
+++ frontend/src/components/taskForm.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export const TaskForm = () => {
+    const [task, setTask] = useState({
+        title: '',
+        description: '',
+        dueDate: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setTask(prev => ({ ...prev, [name]: value }));
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(task);
+        
+        setTask({ title: '', description: '', dueDate: '' });
+    }
+
     return (
-        <form className='TaskForm'>
+        <form className='TaskForm' onSubmit={handleSubmit}>
             <h2>Nova Tarefa</h2>
-            <input type='text' className='task-input' 
-            placeholder='Título'/>
 
-            <input type='text' className='task-input' 
-            placeholder='Descrição'/>
+            <input
+                name='title'
+                type='text'
+                className='task-input'
+                placeholder='Título'
+                value={task.title}
+                onChange={handleChange}
+            />
 
-            <input type='date' className='task-input'/>
+            <input
+                name='description'
+                type='text'
+                className='task-input'
+                placeholder='Descrição'
+                value={task.description}
+                onChange={handleChange}
+            />
+
+            <input
+                name='dueDate'
+                type='date'
+                className='task-input'
+                value={task.dueDate}
+                onChange={handleChange}
+            />
 
             <button type='submit' className='task-button'>Salvar</button>
         </form>
